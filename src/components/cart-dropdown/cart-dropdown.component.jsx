6770fff872a86e2ecd2459108937a3085fbca701
catOWO2011 <catOWO2011@gmail.com>
2022-11-01 20:20:47 -0400

    Added Cart Item logic

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,25 @@
-import Button from "../button/button.component";
-import "./cart-dropdown.styles.scss";
+import { useContext } from "react";
 
-const CardDropdown = () => (
-  <div className="cart-dropdown-container">
-    <div className="cart-items" />
-    <Button>GO TO CHECKOUT</Button>
-  </div>
-);
+import Button from "../button/button.component";
+
+import { CartContext } from "../../contexts/cart.context";
+
+import "./cart-dropdown.styles.scss";
+import CartItem from "../cart-item/cart-item.component";
+
+const CardDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CardDropdown;
