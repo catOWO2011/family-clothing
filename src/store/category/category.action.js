d2045b3aaca5b3f867360ed6b5030c1780d5218a
catOWO2011 <catOWO2011@gmail.com>
2023-01-18 21:59:07 -0400

    Added redux-saga logic for categories

--- src/store/category/category.action.js
+++ src/store/category/category.action.js
@@ -1,6 +1,5 @@
 import { createAction } from "@reduxjs/toolkit";
 
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { CATEGORY_ACTION_TYPES } from "./category.types";
 
 export const fetchCategoriesStart = createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_START);
@@ -8,13 +7,3 @@ export const fetchCategoriesStart = createAction(CATEGORY_ACTION_TYPES.FETCH_CAT
 export const fetchCategoriesSuccess = createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS);
 
 export const fetchCategoriesFailed = createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_FAILED);
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  dispatch(fetchCategoriesStart());
-  try {
-    const categories = await getCategoriesAndDocuments();
-    dispatch(fetchCategoriesSuccess(categories))
-  } catch (error) {
-    dispatch(fetchCategoriesFailed(error));
-  }
-};
